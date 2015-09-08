fd3f641ff3027819a830cd70def3d77e2d8ebdfb
Rasmus Pind <rasmus.pind@khk.ee>
2015-09-08 09:36:52 +0300

    1420586-102650530 pealehe loomine

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>ProjectX</title>
+</head>
+<body>
+<img src="assets/img/banner.png" style="width:1350px;height:250px;" >
+<hr>
+
+<a href="/index.php">Minust</a>
+<a href="/too2.php">Töö #2</a>
+
+<br>
+<br>
+
+<H1>Mulle meeldib süüa</H1>
+<ol>
+    <li>Õunu</li>
+    <li>Banaane</li>
+    <li>Pirne</li>
+</ol>
+
+Täna  on <?php echo date("l jS \of F Y"); ?>
+
+
+</body>
+</html>
