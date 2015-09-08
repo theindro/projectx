b9f2f56ed11236fa6f42cda24e350295a6be4003
Rasmus Pind <rasmus.pind@khk.ee>
2015-09-08 09:53:32 +0300

    1420586-102882178 h2 p tekst

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
 <h1>Tere!</h1>
 <p>Minu nimi on Indro Malleus ja Te olete sattunud minu veebilehele.</p>
 <h2>Enesetutvustus</h2>
+<ul>
+    <li>Olen 20 aastane noormees</li>
+    <li>Pikkus 185cm</li>
+    <li>Kaal 66kg</li>
+    <li>Tähtkuju Jäär</li>
+</ul>
 <h3>Haridustee</h3>
 <h4>Ajateenistus</h4>
 <h5>Hobid</h5>
