592d55ea498860c6f1f9159b5461ff0c84c4d5c2
Rasmus Pind <rasmus.pind@khk.ee>
2015-09-09 12:27:56 +0300

    1420586-102897730 kasuta CSS

---
+++ too2.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <style>
+    </style>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>Project X</title>
+</head>
+<body>
+<img src="assets/img/banner.png" style="width:1346px;height:250px;" >
+<hr>
+<br>
+
+<a href="/index.php"><span><div2>..:::Minust:::..</div2></span></a>
+<a href="/too2.php"><span><div2>..:::Link #2:::..</div2></span></a>
+<a href="/too3.php"><span><div2>..:::Link #3:::..</div2></span></a>
+<a href="/too4.php"><span><div2>..:::Link #4:::..</div2></span></a>
+<a href="/too5.php"><span><div2>..:::Link #5:::..</div2></span></a>
+
+<br>
+<br>
+
+<div>
+
+   <p>LEHT ON VALMIMISEL</p>
+</div>
+
+<hr>
+<!-- Lisan kuupäeva! -->
+<p id="kuu"><?php echo date("l jS \of F Y"); ?></p>
+
+<p id="pind">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspRasmus Pind©</p>
+
+
+</body>
+</html>
